ccf2addc8b8fd38c55f849d383d5e0e738024762
Tamara Naumovic <tamara.naumovic@gmail.com>
2022-10-24 21:17:26 +0200

    pretraga prodavnice proizvoda

---
+++ prodavnica.php
@@ -0,0 +1,65 @@
+<?php
+$niz_proizvoda = array(
+    array(
+        "id"=>1,
+        "name"=>"Laptop 1",
+        "price"=>1500
+    ),
+    array(
+        "id"=>2,
+        "name"=>"Laptop 2",
+        "price"=>1250
+    ),
+    array(
+        "id"=>3,
+        "name"=>"Laptop 3",
+        "price"=>1000
+    ),
+    array(
+        "id"=>4,
+        "name"=>"Keyboard 1",
+        "price"=>150
+    ),
+    array(
+        "id"=>5,
+        "name"=>"Keyboard 2",
+        "price"=>100
+    ),
+    array(
+        "id"=>6,
+        "name"=>"Keyboard 3",
+        "price"=>50
+    ),
+    array(
+        "id"=>7,
+        "name"=>"Monitor 1",
+        "price"=>400
+    ),
+    array(
+        "id"=>8,
+        "name"=>"Monitor 2",
+        "price"=>250
+    )
+    );
+
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+    if(isset($_GET["name"])){
+        $search_proizvodi = array();
+        $search_q = htmlspecialchars($_GET["name"]);
+        $search_q = trim($search_q);
+        $search_q = strtolower($search_q);
+        foreach($niz_proizvoda as $proizvod){
+            if(str_contains(strtolower($proizvod["name"]), $search_q)){
+                $search_proizvodi[]=$proizvod;
+            }
+        }
+        require __DIR__. "/search/search.php";
+        exit();
+    }else{
+        require __DIR__. "/search/katalog.php";
+        exit();
+    }
+}
+
+
+?>
---
+++ search/katalog.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Katalog</title>
+</head>
+<body>
+
+<h3>Proizvodi</h3>
+<table border="1">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Price</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php 
+            foreach($search_niz as $pr):
+        ?>
+        <tr>
+            <td><?php echo $pr['id'];?></td>
+            <td><?php echo $pr['name'];?></td>
+            <td><?php echo $pr['price'];?></td>
+        </tr>
+        <?php endforeach;?>
+        <!-- <tr>
+            <td>2</td>
+            <td>Keyboard</td>
+            <td>150</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Monitor</td>
+            <td>400</td>
+        </tr> -->
+    </tbody>
+</table>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <input type="text" name="name" id="name" placeholder="Search by name: ">
+    <input type="submit" value="Search" name="search">
+
+</form>
+    
+</body>
+</html>
