571812e00ede577d3cc33ce71526b9aebc0bd345
Tamara Naumovic <tamara.naumovic@gmail.com>
2022-10-24 19:13:06 +0200

    validacija forme

--- index.php
+++ index.php
@@ -7,7 +7,26 @@
     <title>Form</title>
 </head>
 <body>
-    <form action="welcome.php" method="POST">
+<?php
+    $name="";
+    $email="";
+    $comment = "";
+    $gender = "";
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+            $name = test_inputs($_POST["name"]);
+            $email = test_inputs($_POST["email"]);
+            $comment = test_inputs($_POST["comment"]);
+            $gender = test_inputs($_POST["gender"]);
+        }
+
+        function test_inputs($data){
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+    ?>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
     <!-- default method GET -->
     <!-- <form action="welcome.php" > -->
         <label for="name">Name: </label>
@@ -16,10 +35,23 @@
         <input type="email" name="email" id="email" placeholder="Enter your email: "><br>
         <label for="comment">Comment:</label>
         <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+        <br>
+        <label >Gender: </label>
+        <input type="radio" name="gender" id="m" value="male"> Male
+        <input type="radio" name="gender" id="f" value="female"> Female
+        <input type="radio" name="gender" id="o" value="other"> Other
         <br>    
         <input type="submit" value="Enter meeting">
     </form>
-
+    <h2>Your input: </h2>
+    <p>
+        <?php
+        echo $name."<br>";
+        echo $email."<br>";
+        echo $comment."<br>";
+        echo $gender."<br>";
+        ?>
+    </p>
 
 </body>
 </html>
