5a686da43cef70fcf71ace8f0d20ee7029951239
Tamara Naumovic <tamaranaumovic@gmail.com>
2022-12-14 19:14:16 +0100

    insertOne test

--- test.php
+++ test.php
@@ -1,5 +1,25 @@
 <?php
 // testiranje MongoDB konekcije
+// require_once __DIR__.'/vendor/autoload.php';
+// echo __DIR__; //putanja do roott direktorijuma (npr > C:\xampp\htdocs\undp\mongo-proj)
+require_once 'vendor/autoload.php';
 
+$dbhost = "localhost:27017";
+$dbname = "test";
+
+$db_client = new MongoDB\Client("mongodb://$dbhost");
+
+// $db = $db_client->test;
+$db = $db_client->$dbname;
+$coll_users = $db->users;
+
+// $insertOneRez = $coll_users->insertOne([
+//     'username'=>'admin',
+//     'email'=>'admin@email.com',
+//     'name'=> 'Admin name'
+// ]);
+
+
+printf("Ubaceno je %d dokumenata\n", $insertOneRez->getInsertedCount());
 
 ?>
