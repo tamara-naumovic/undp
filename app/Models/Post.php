b194ab3cb21cbcf182036388898dcfd8f69a3b0c
Tamara Naumovic <tamaranaumovic@gmail.com>
2023-03-01 21:39:56 +0100

    link category post and user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,5 +15,10 @@ class Post extends Model
         'body'
     ];
 
-    
+    public function category(){
+        return $this->belongsTo(Category::class, "category_id");
+    }
+    public function user(){
+        return $this->belongsTo(User::class, "user_id");
+    }
 }
