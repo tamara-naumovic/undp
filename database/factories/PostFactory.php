e97cf2f001c7f13f58f42232586e69708b7613e1
Tamara Naumovic <tamara.naumovic@gmail.com>
2023-03-03 22:43:41 +0100

    kreirani factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,12 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title'=>fake()->title(),
+            'excerpt'=>fake()->sentence(),
+            'body'=>fake()->paragraph(10),
+            'slug'=>fake()->unique()->slug(),
+            'category_id'=>Category::factory(),
+            'user_id'=>User::factory()
         ];
     }
 }
