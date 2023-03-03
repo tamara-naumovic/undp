9428a51525e3d487592082be67fd4bb701b7e2fd
Tamara Naumovic <tamara.naumovic@gmail.com>
2023-03-03 22:43:51 +0100

    seedovani podaci

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Database\Seeder;
 use App\Models\User;
@@ -19,11 +20,43 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password'=>'test123'
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        //     'password'=>'test123'
+        // ]);
+
+        // $cat4 = Category::create([
+        //     'name'=>'Test',
+        //     'slug'=>'test'
+        // ]);
+        $user = User::find(1);
+        $user1 = User::find(2);
+        $user2 = User::find(3);
+        // $cat = User::find(4);
+        // $post = Post::create([
+        //     'title'=>'Test post 2',
+        //     'excerpt'=>'test isecak 2',
+        //     'body'=>'test body 2',
+        //     'slug'=>'test-post-2',
+        //     'category_id'=>$cat->id,
+        //     'user_id'=>$user->id
+        // ]);
+
+        $cat1 = Category::factory()->create();
+        $cat2 = Category::factory()->create();
+
+        Post::factory(5)->create([
+            'user_id'=>$user->id,
+            'category_id'=>$cat1->id,
         ]);
+
+        Post::factory(5)->create([
+            'user_id'=>$user2->id,
+            'category_id'=>$cat2->id,
+        ]);
+
+
 
     }
 }
