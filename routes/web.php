1e7de9a27b569dabeeaa1b2fa7c73aae0346f113
Tamara Naumovic <tamara.naumovic@gmail.com>
2023-03-03 22:45:47 +0100

    redefinisane rute zbog nivoa pristupa u putanjama

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog',[PostsController::class,'index']); //prikazuje sve 
+Route::get('/blog/{id}',[PostsController::class,'show']); //prikazuje jedan blog iz baze
+Route::get('/blog/create/post', [PostsController::class,'create']); // prikazuje create formu
+Route::post('/blog/create/post', [PostsController::class,'store']); // cuva kreirani post u bazu
+Route::get('/blog/{id}/edit', [PostsController::class,'edit']); // prikazuje edit formu
+Route::put('/blog/{id}/edit', [PostsController::class,'update']); // cuva azurirani post u bazu
+Route::delete('/blog/{id}',[PostsController::class,'delete']); // brise post iz baze
+
